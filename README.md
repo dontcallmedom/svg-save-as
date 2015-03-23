3fb132a26f9de61d4a6d7c79375c592ecdf5fef3
Dominique Hazael-Massieux <dom@w3.org>
2015-03-23 23:06:52 +0100

    change API to work on nodes rather than selectors

--- README.md
+++ README.md
@@ -2,13 +2,13 @@ When SVG is used in-line in HTML, it is today not possible for users to save the
 
 [svg-save-as.js](svg-save-as.js) defines a function SVGSaveAs that takes two parameters:
 
-* a CSS selector
+* a pointer to an SVG node
 * an image format (default to "image/png")
 
-When that function is invoked, it emulates the "save image" feature for all svg elements matching the selector, providing it as an image of the said format. [See it in action](https://dontcallmedom.github.io/svg-save-as/example/index.html).
+When that function is invoked, it emulates the "save image" feature for the svg element, providing it as an image of the said format. [See it in action](https://dontcallmedom.github.io/svg-save-as/example/index.html).
 
 # How does it work?
-For each matched svg element, the library overlays an invisible `<img>` tag. That image source is set to:
+The library overlays an invisible `<img>` tag over the SVG node. That image source is set to:
 
 * the content of the svg element (transformed into a `data:` URL) if the output format is set to `image/svg+xml`
 * a `canvas`-generated `data:` URL of the svg content read in an image using the trick above
--- svg-save-as.js
+++ svg-save-as.js
@@ -1,50 +1,45 @@
-function SVGSaveAs(selector, format) {
-    function screenshotUpdater(svg, screenshot) {
-        return function() {
-            screenshot.src = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svg.outerHTML)));
-        };
+function SVGSaveAs(node, format) {
+    function screenshotUpdater() {
+        screenshot.src = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(node.outerHTML)));
     }
 
     format = format || "image/png";
 
-    selector = selector || "svg";
-    [].forEach.call(document.querySelectorAll(selector), function (s) {
-        var div = document.createElement("div");
-        div.style.margin = 0;
-        div.style.padding = 0;
-        div.style.position = "relative";
-        s.parentNode.replaceChild(div, s);
-        div.appendChild(s);
-        if (!s.getAttribute("xmlns")) {
-            s.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    var div = document.createElement("div");
+    div.style.margin = 0;
+    div.style.padding = 0;
+    div.style.position = "relative";
+    node.parentNode.replaceChild(div, node);
+    div.appendChild(node);
+    if (!node.getAttribute("xmlns")) {
+        node.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    }
+    var screenshot = new Image();
+
+    var img = new Image();
+    var canvas = document.createElement("canvas");
+    img.width = node.width.baseVal.value;
+    img.height = node.height.baseVal.value;
+    screenshot.width = img.width;
+    screenshot.height = img.height;
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    img.style.position = "absolute";
+    img.style.top = 0;
+    img.style.left = 0;
+    img.style.opacity = 0;
+
+    screenshot.addEventListener("load", function() {
+        if (format === "image/svg+xml") {
+            img.src = screenshot.src;
+        } else {
+            canvas.getContext("2d").drawImage(screenshot, 0, 0);
+            img.src = canvas.toDataURL(format);
         }
-        var screenshot = new Image();
-
-        var img = new Image();
-        var canvas = document.createElement("canvas");
-        img.width = s.width.baseVal.value;
-        img.height = s.height.baseVal.value;
-        screenshot.width = img.width;
-        screenshot.height = s.height.baseVal.value;
-        canvas.width = img.width;
-        canvas.height = img.height;
-
-        img.style.position = "absolute";
-        img.style.top = 0;
-        img.style.left = 0;
-        img.style.opacity = 0;
-
-        screenshot.addEventListener("load", function() {
-            if (format === "image/svg+xml") {
-                img.src = screenshot.src;
-            } else {
-                canvas.getContext("2d").drawImage(screenshot, 0, 0);
-                img.src = canvas.toDataURL(format);
-            }
-        });
-
-        img.addEventListener("mouseenter", screenshotUpdater(s, screenshot));
-        img.addEventListener("touchstart", screenshotUpdater(s, screenshot));
-        div.appendChild(img);
     });
+
+    img.addEventListener("mouseenter", screenshotUpdater);
+    img.addEventListener("touchstart", screenshotUpdater);
+    div.appendChild(img);
 }
